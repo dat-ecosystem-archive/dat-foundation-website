8564867b59d0ea4ae87a220350921c71a9794bde
whoisgina <whoisgina@me.com>
2019-09-26 10:31:49 -0700

    add person component
    takes a person's name and displays the rest of their info from the collection entry

---
+++ site/.vuepress/components/Person.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="person">
+    <a :href="person.website" class="person__name">{{ person.name }}</a>
+    <div class="person__title">{{ person.title }}</div>
+    <a :href="person.affiliation.url" class="person__affiliation">{{ person.affiliation.name }} </a>
+   <br/><br/>
+   <!-- <a :href="person.website"><img :src="person.image"></a> -->
+   <div class="person__bio">{{ person.bio }}</div>
+    <br/><br/><br/>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'Person',
+    props: {
+      name: {
+        type: String,
+        required: false
+      }
+    },
+
+    computed: {
+      person(name) {
+        return this.$site.pages
+          .filter(x => x.path.startsWith('/people/') && x.frontmatter.name == this.name)[0].frontmatter
+      }
+    }
+  }
+</script>

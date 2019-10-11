5ea14e35030796f8eb40fc55fd47d1d7113cdadc
whoisgina <whoisgina@me.com>
2019-10-11 13:58:00 -0700

    rough blog styling

--- site/.vuepress/components/BlogIndex.vue
+++ site/.vuepress/components/BlogIndex.vue
@@ -1,13 +1,27 @@
 <template>
   <div class="blog__index">
+    <h1 class="blog__index__page-title page-title">Blog</h1>
+
     <div v-for="post in journal" :key="post.title" class="blog__index__post">
-      <a tag="h3" :href="post.path" class="title">{{ post.frontmatter.title }}</a>
-      <p>{{ post.frontmatter.excerpt }}</p>
+      <img :src="post.frontmatter.image"
+           class="blog__index__post__image" />
+
+      <div class="blog__index__post__info">
+        
+        <router-link tag="h3" :to="post.path" class="blog__index__post__title">{{ post.frontmatter.title }}</router-link>
+        <p class="blog__index__post__excerpt" v-html="markdown(post.frontmatter.excerpt)"></p>
+        <div class="blog__index__post__authors">
+          <div v-for="author in post.frontmatter.authors">
+            {{ author }}
+          </div>
+        </div>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
+  import { markdown } from '../util'
   export default {
     computed: {
       journal() {
@@ -15,16 +29,66 @@
           .filter(x => x.path.startsWith('/blog/posts'))
           .sort((a, b) => new Date(b.frontmatter.date) - new Date(a.frontmatter.date))
       }
-    }
+    },
+    methods: { markdown }
   }
 </script>
 
 <style lang="scss">
+@import '../assets/stylesheets/variables.scss';
+
 .blog {
   &__index {
-    &__post {
-      margin-bottom: 2rem;
+    display: flex;
+    flex-wrap: wrap;
+
+    &__page-title {
+      margin-bottom: $space-medium;
+      width: 100%;
     }
+
+    &__post {
+      @include shadow;
+      overflow: hidden;
+      margin-bottom: $space-large;
+      border: 0.5px solid $color-gray-light;
+      // padding: $space-base;
+      width: 30%;
+      margin-right: $space-small;
+      transition: all 250ms ease-in-out;
+
+
+      &:hover {
+        transform: translateY(-$space-small);
+        cursor: pointer;
+      }
+
+      &:last-of-type {
+        margin-right: 0;
+      }
+
+      &__title {
+        margin-bottom: $space-base;
+      }
+
+      &__image {
+        
+      }
+
+      &__info {
+        padding: $space-base;
+      }
+
+      &__excerpt {
+        margin-bottom: $space-medium;
+      }
+      
+      &__authors {
+        @include type-small;
+        color: $color-green-dark;
+      }
+    }
+
   }
 }
 </style>
--- site/blog/posts/2019-10-08-test-entry.md
+++
@@ -1,6 +0,0 @@
----
-title: Test entry
-content: Some content.
-date: '2019-10-08'
----
-
